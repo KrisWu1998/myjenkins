d8e29f4b5fb5bdb564f26412d04f8da335da910b
吴泽钒 <727980015@qq.com>
2022-07-02 16:34:58 +0800

    feat: 新增jenkins部署文件

---
+++ jenkins.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -ilex
+
+echo "开始执行了vue脚本文件"
+
+echo "开始下载node_module"
+npm install
+
+echo "开始打包"
+npm run build
+
+echo "清空nginx下的文件"
+rm -rf '/usr/share/nginx/html/dist'
+
+echo "将jenkins打包的文件拷贝到nginx下"
+cp -r '/var/lib/jenkins/workspace/mywebsite/dist' '/usr/share/nginx/html/dist'
